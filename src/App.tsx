531a57cdb2ce6895caa1bf3a6751f1c3d815a5cf
kevin1793 <kevin1793@gmail.com>
2023-08-04 02:09:33 -0500

    1 file modified, 1 added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import './style.css';
 
+import Middle from './components/middle';
+
 export default function App() {
   return (
-    <div>
+    <>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-    </div>
+      <Middle/>
+    </>
   );
 }
---
+++ src/components/middle.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function Middle() {
+  return (
+    <>
+      <div>HI?????????</div>
+    </>
+  );
+}
+
+export default Middle;
