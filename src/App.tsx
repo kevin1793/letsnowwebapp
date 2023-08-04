657927e9ce83a6fd108e7d26c41cf5e10fcd4016
kevin1793 <kevin1793@gmail.com>
2023-08-04 02:39:51 -0500

    3 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,7 @@ import Middle from './components/middle';
 export default function App() {
   return (
     <>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <Middle/>
+      <Middle />
     </>
   );
 }
--- src/components/middle.tsx
+++ src/components/middle.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react';
 
 function Middle() {
+  function submitPost(e: any) {
+    var postVal = document.getElementById('postText').value;
+    console.log(postVal);
+    var feedContNode = document.getElementById('feedCont');
+
+    var newHTML = 'hi';
+
+    feedContNode.innerHTML += newHTML;
+  }
   return (
     <>
-      <div>HI?????????</div>
+      <div id="tweetCont">
+        <div></div>
+        <textarea id="tweetText"></textarea>
+        <button onClick={(e) => submitPost(e)}>Make POST</button>
+      </div>
+      <div id="feedCont">
+        <div className="postBox">
+          <img src="" />
+          <div className="username">Kevvey</div>
+          <div className="username">Kevvey</div>
+        </div>
+      </div>
     </>
   );
 }
